a3d258d5d10917321f7690220e8fe56f98a04d95
Şuayip Emre <suayipemredev@gmail.com>
2024-07-15 01:25:21 +0300

    feat: add responsive menu design for mobile and desktop views

--- src/components/SignUpNowButton/index.tsx
+++ src/components/SignUpNowButton/index.tsx
@@ -6,8 +6,8 @@ type Props = {
 }
 const SignUpNowButton : React.FC<Props> = ({isWhiteBorder}) => {
   return (
-    <button className={`isWhiteBorder ? 'border-white' : 'border-2 border-[#78350F]'} rounded-lg py-5 px-6` }>
-    <p className="text-[#78350F] text-[20px] font-medium leading-6 tracking-[0.5px]">
+    <button className={`${isWhiteBorder ?  'border-white' : 'border-[#78350F]'}  border-2 rounded-lg py-5 px-6` }>
+    <p className={` ${isWhiteBorder ? 'text-white' : 'text-[#78350F]'} text-[20px] font-medium leading-6 tracking-[0.5px]`}>
       Sign up now
     </p>
   </button>
--- src/components/header/NavbarMenu.tsx
+++ src/components/header/NavbarMenu.tsx
@@ -1,12 +1,20 @@
-import MenuIcon from '@/icons/MenuIcon'
+
 import React from 'react'
 import SignUpNowButton from '../SignUpNowButton'
+import MenuIcon from '@/icons/MenuIcon'
+import CloseIcon from '@/icons/CloseIcon'
 
-const NavbarMenu = () => {
+
+type Props = {
+    isMobileMenuOpen: boolean
+    setIsMobileMenuOpen: (isOpen: boolean) => void
+}
+const NavbarMenu: React.FC<Props> = ({ isMobileMenuOpen, setIsMobileMenuOpen }) => {
     return (
-        <div>
+        <div >
 
-            <ul className='hidden 2xl:flex items-center gap-10 '>
+            
+            <ul className='hidden xl:flex items-center gap-10 '>
                 <li className='text-[#78350F] text-base leading-6 font-medium'>Products</li>
                 <li className='text-[#78350F] text-base leading-6 font-medium'>Solutions</li>
                 <li className='text-[#78350F] text-base leading-6 font-medium'>Pricing</li>
@@ -15,9 +23,28 @@ const NavbarMenu = () => {
                 <SignUpNowButton />
             </ul>
 
-            <div className='flex 2xl:hidden'>
+
+            <div className='flex xl:hidden cursor-pointer' onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
                 <MenuIcon />
             </div>
+
+            {
+                isMobileMenuOpen && <div className='flex absolute top-0 gap-10 right-0 z-10  p-20  flex-col  sm:w-[80%] md:w-[50%] xl:hidden bg-white shadow-black-strong '>
+                    <ul className='flex flex-col gap-10'>
+                        <li className='text-dark-blue text-base leading-6 font-medium'>Products</li>
+                        <li className='text-dark-blue text-base leading-6 font-medium'>Solutions</li>
+                        <li className='text-dark-blue text-base leading-6 font-medium'>Pricing</li>
+                        <li className='text-dark-blue text-base leading-6 font-medium'>Resources</li>
+                        <li className='text-dark-blue text-base leading-6 font-medium'>Log In</li>
+                    </ul>
+                    <SignUpNowButton  />
+
+                    <div className='absolute top-10 right-10 cursor-pointer' onClick={() => setIsMobileMenuOpen(false)}>
+                        <CloseIcon  />
+                    </div>
+                </div>
+            }
+
         </div>
     )
 }
